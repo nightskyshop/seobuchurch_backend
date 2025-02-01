f606df88262d05ddb1d71bfbe943c5f860d43259
nightskyshop <nightskyshop1023@naver.com>
2025-02-01 17:39:46 +0900

    Fix: Delete content limit

--- src/main/java/com/example/seobuchurch/Entity/ColumnEntity.java
+++ src/main/java/com/example/seobuchurch/Entity/ColumnEntity.java
@@ -1,9 +1,6 @@
 package com.example.seobuchurch.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +12,7 @@ public class ColumnEntity extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String content;
     private String author;
 }
